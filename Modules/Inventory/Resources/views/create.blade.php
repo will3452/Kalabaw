43194ca77171802df6aee04709a79b553ed2f38e
William Galas <williamgalas2@gmail.com>
2022-05-20 12:15:47 +0800

    Update create.blade.php

--- Modules/Inventory/Resources/views/create.blade.php
+++ Modules/Inventory/Resources/views/create.blade.php
@@ -6,6 +6,7 @@
                 @if (! isExcludeToForm($c, $model))
                     @if (isSelectField($c, $model))
                         <x-form.select name="{{$c}}" label="{{getFieldLabel($c)}}">
+                            <option value="N/a">N/a</option>
                             @foreach (getFieldsOption($c, $model) as $key=>$value)
                                 <option value="{{$key}}">{{$value}}</option>
                             @endforeach
